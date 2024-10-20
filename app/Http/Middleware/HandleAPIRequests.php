1b4ac2634cddd8b8a98aaa3ad8e005413e023896
Marator2K07 <dmitriyshaaa@gmail.com>
2024-10-20 17:01:36 +0300

    Создание посредника обработки запросов к АПИ с предполагаемой реализацией.

---
+++ app/Http/Middleware/HandleAPIRequests.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class HandleAPIRequests
+{
+    public function handle(Request $request, Closure $next): Response
+    {
+        $requestPath = $request->path();
+
+        // если запрос к АПИ идет по путям, не требующих авторизации
+        if (
+            strpos($requestPath, 'constants.API.API_BOOKS_GET_START_PATH') !== false
+            || strpos($requestPath, 'constants.API.API_AUTHORS_GET_START_PATH') !== false
+            || strpos($requestPath, 'constants.API.API_AUTHORS_GET_START_PATH') !== false
+        ) {
+            return $next($request);
+        }
+        // все остальные пути требуют авторизации
+        if (Auth::check()) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        return $next($request);
+    }
+}
