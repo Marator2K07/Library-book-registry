8a440542d8d6e2afc2c8d14d98e92f1b19ea5795
Marator2K07 <dmitriyshaaa@gmail.com>
2024-10-20 16:59:48 +0300

    Добавление стартовых констант в проект и их подключение.

---
+++ config/constants.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'API' => [
+        'API_BOOKS_GET_START_PATH' => '/api/books/get',
+        'API_BOOKS_UPDATE_START_PATH' => '/api/books/update',
+        'API_BOOKS_DELETE_START_PATH' => '/api/books/delete',
+        'API_AUTHORS_GET_START_PATH' => '/api/authors/get',
+        'API_AUTHORS_UPDATE_START_PATH' => '/api/authors/update',
+        'API_GENRES_UPDATE_START_PATH' => '/api/genres/get'
+    ]
+];
