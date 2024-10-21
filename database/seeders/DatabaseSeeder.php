0cf00a080620ae235c03997e1c568ade0c36ac51
Marator2K07 <dmitriyshaaa@gmail.com>
2024-10-21 11:46:18 +0300

    Стартовый тестовый код для сидера.
    Создаются пользователи и вяжутся с авторами.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Author;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +14,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
+        // сначала добавляем админа и десять других пользователей
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => 'rootDB12345'
         ]);
+        User::factory(10)->create();
+        // связываем юзеров с авторами
+        $users = User::all();
+        foreach ($users as $user) {
+            Author::factory()->create([
+                'name' => $user->name,
+                'user_id' => $user->id
+            ]);
+        }
     }
 }
