226dff88d25c6dc1080da13cfac612ffafa4a3ea
Marator2K07 <dmitriyshaaa@gmail.com>
2024-10-21 15:32:20 +0300

    Следующая версия сидера.
    Заполнение всех таблиц задания данными.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\Author;
+use App\Models\Book;
+use App\Models\Genre;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -20,7 +22,7 @@ class DatabaseSeeder extends Seeder
             'email' => 'admin@example.com',
             'password' => 'rootDB12345'
         ]);
-        User::factory(10)->create();
+        User::factory(55)->create();
         // связываем юзеров с авторами
         $users = User::all();
         foreach ($users as $user) {
@@ -28,6 +30,20 @@ class DatabaseSeeder extends Seeder
                 'name' => $user->name,
                 'user_id' => $user->id
             ]);
+        }
+        // добавляем книги для авторов
+        for ($i = 0; $i < 555; $i++) {
+            Book::factory()->create([
+                'author_id' => fake()->numberBetween(1, 55)
+            ]);
+        }
+        // пусть будет 33 жанра
+        Genre::factory(33)->create();
+        // ставим жанры книгам или книгам жанры
+        $genres = Genre::all();
+        $books = Book::all();
+        foreach ($books as $book) {
+            $book->genres()->attach($genres->random()->id);
         }
     }
 }
