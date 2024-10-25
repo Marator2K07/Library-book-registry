0d472e3e84dd81c4f99580792d6254b3a9301f4f
Marator2K07 <dmitriyshaaa@gmail.com>
2024-10-25 20:10:55 +0300

    Правки в файле констант + добавление новой.

--- config/constants.php
+++ config/constants.php
@@ -3,7 +3,9 @@
 define('BOOKS_GET_START_PATH', '/books/get');
 define('BOOKS_UPDATE_START_PATH', '/books/update');
 define('BOOKS_DELETE_START_PATH', '/books/delete');
-define('AUTHORS_AUTH_START_PATH', '/author/auth');
+define('AUTHORS_AUTH_PATH', '/author/auth');
 define('AUTHORS_GET_START_PATH', '/authors/get');
 define('AUTHORS_UPDATE_START_PATH', '/authors/update');
 define('GENRES_GET_START_PATH', '/genres/get');
+
+define('DEFAULT_PAGINATE_VALUE', 15);
