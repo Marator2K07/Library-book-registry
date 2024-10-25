476f857c25f3efe137ad2aec3e04bec32b499118
Marator2K07 <dmitriyshaaa@gmail.com>
2024-10-25 10:37:49 +0300

    Исправленная версия посредника API запросов.

--- app/Http/Middleware/HandleAPIRequests.php
+++ app/Http/Middleware/HandleAPIRequests.php
@@ -2,18 +2,18 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\User;
 use Closure;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
+use Laravel\Sanctum\PersonalAccessToken;
 use Symfony\Component\HttpFoundation\Response;
 
 class HandleAPIRequests
 {
     public function handle(Request $request, Closure $next): Response
     {
-        $requestPath = $request->path();
-
         // если запрос к АПИ идет по путям, не требующих авторизации
+        $requestPath = $request->path();
         if (
             strpos($requestPath, constant('BOOKS_GET_START_PATH')) !== false
             || strpos($requestPath, constant('AUTHORS_GET_START_PATH')) !== false
@@ -22,10 +22,18 @@ class HandleAPIRequests
         ) {
             return $next($request);
         }
-        // все остальные пути требуют авторизации
-        if (!Auth::check()) {
-            return response()->json(['error' => 'Unauthorized'], 401);
+        // (все остальные пути требуют авторизации)
+        // пытаемся получить экземпляр токена
+        $tokenValue = $request->header('Authorization');
+        $token = PersonalAccessToken::findToken(explode(" ", $tokenValue)[1]);
+        if (!$tokenValue || !$token) {
+            return response()->json(['error' => 'Unauthorized access attempt'], 401);
         }
+        // если все успешно - то пишем важные данные аутентификации в заголовки
+        // и обновляем последнее использование токена
+        $request->headers->set('Auth-User', User::find($token->tokenable_id));
+        $token->last_used_at = now();
+        $token->save();
 
         return $next($request);
     }
