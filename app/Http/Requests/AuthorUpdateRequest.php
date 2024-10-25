207e4834eef3716f1157a19bfab4ccaef69774f9
Marator2K07 <dmitriyshaaa@gmail.com>
2024-10-25 20:56:48 +0300

    Создан валидатор запросов обновления для автора.
    с начальной, предполагаемой реализацией.

---
+++ app/Http/Requests/AuthorUpdateRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+
+class AuthorUpdateRequest extends FormRequest
+{
+    public $validator = null;
+
+    /**
+     * В случае ошибки валидации, мы сможем получить доступ к ошибкам
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        $this->validator = $validator;
+    }
+
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'string|max:255|min:2',
+            'day_of_birth' => 'before:now|date_format:Y-m-d'
+        ];
+    }
+}
