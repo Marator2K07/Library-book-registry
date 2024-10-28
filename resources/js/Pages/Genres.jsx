849708549f7e5b227c37eb7cab996939e3f389f5
Marator2K07 <dmitriyshaaa@gmail.com>
2024-10-28 11:44:41 +0300

    Теперь на странице с жанрами можно успешно удалять выбранный.
    то есть, добавлена форма удаления и управление ее показом.

--- resources/js/Pages/Genres.jsx
+++ resources/js/Pages/Genres.jsx
@@ -4,14 +4,26 @@ import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout'
 import { Head, usePage } from '@inertiajs/react'
 import React, { useState } from 'react'
 import CreateGenreForm from './Profile/Partials/Genre/CreateGenreForm';
+import { Transition } from '@headlessui/react';
+import DeleteGenreForm from './Profile/Partials/Genre/DeleteGenreForm';
 
 export default function Genres() {
     const { genres, links } = usePage().props;
+    const [idForDeletion, setIdForDeletion] = useState(null);
+    const [showModal, setShowModal] = useState(false);
     const [showCreateGenreForm, setShowCreateGenreForm] = useState(false);
+    const [showDeleteGenreForm, setShowDeleteGenreForm] = useState(false);
 
     // управление показом формы создания нового жанра
     const handleCreateGenreForm = () => {
         setShowCreateGenreForm(!showCreateGenreForm);
+        setShowModal(!showModal);
+    };
+
+    // управление показом формы удаления жанра
+    const handleDeleteGenreForm = () => {
+        setShowDeleteGenreForm(!showDeleteGenreForm);
+        setShowModal(!showModal);
     };
 
     if (genres && links) {
@@ -45,7 +57,12 @@ export default function Genres() {
                                         <td className="p-1">{genre.name}</td>
                                         <td className="p-1">
                                             <PrimaryButton className="btn btn-info mr-2">Update</PrimaryButton>
-                                            <DangerButton>Delete</DangerButton>
+                                            <DangerButton action={() => {
+                                                handleDeleteGenreForm();
+                                                setIdForDeletion(genre.id);
+                                            }}>
+                                                Delete
+                                            </DangerButton>
                                         </td>
                                     </tr>
                                 ))}
@@ -53,20 +70,36 @@ export default function Genres() {
                         </table>
                     </div>
 
-                    <div className="absolute dark:bg-gray-700 ring-1 rounded-lg">
-                        <CreateGenreForm
-                            className="container flex flex-col w-max rounded-lg bg-white p-6 m-5 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] dark:bg-gray-800 dark:ring-red-700"
-                            hidden={showCreateGenreForm}
-                            setHidden={handleCreateGenreForm}
-                        />
-                    </div>
+                    {
+                        <Transition
+                            show={showModal}
+                            enter="transition ease-in-out"
+                            enterFrom="opacity-0"
+                            leave="transition ease-in-out"
+                            leaveTo="opacity-0"
+                        >
+                            <div className="absolute dark:bg-gray-700 ring-1 rounded-lg">
+                                <CreateGenreForm
+                                    className="container flex flex-col w-max rounded-lg bg-white p-6 m-5 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] dark:bg-gray-800 dark:ring-red-700"
+                                    hidden={showCreateGenreForm}
+                                    setHidden={handleCreateGenreForm}
+                                />
+                                <DeleteGenreForm
+                                    className="container flex flex-col w-max rounded-lg bg-white p-6 m-5 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] dark:bg-gray-800 dark:ring-red-700"
+                                    hidden={showDeleteGenreForm}
+                                    setHidden={handleDeleteGenreForm}
+                                    idForDeletion={idForDeletion}
+                                />
+                            </div>
+                        </Transition>
+                    }
 
                     <div className="container flex w-max gap-4 rounded-lg bg-white p-6 m-5 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] dark:bg-gray-800 dark:ring-red-700">
                         {links.previous &&
                             <PrimaryButton className="btn btn-outline-secondary">
                                 <a className="page-link" href={links.previous}>Previous page</a>
                             </PrimaryButton>}
-                        <PrimaryButton onClick={() => handleCreateGenreForm()}>
+                        <PrimaryButton onClick={ () => handleCreateGenreForm() }>
                             New genre
                         </PrimaryButton>
                         {links.next &&
