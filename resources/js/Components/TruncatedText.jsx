08eefdf9e9d1e904d3f695660067d102cb386cab
Marator2K07 <dmitriyshaaa@gmail.com>
2024-10-29 20:47:12 +0300

    Улучшенная версия авто обрезаемого текста + обновлены константы.

--- resources/js/Components/TruncatedText.jsx
+++ resources/js/Components/TruncatedText.jsx
@@ -1,15 +1,25 @@
-import { DEFAULT_MAX_TEXT_LENGTH } from '@/constants'
-import React from 'react'
+import { DEFAULT_MAX_TEXT_LENGTH_RATIO } from '@/constants'
+import React, { useEffect, useState } from 'react'
 
 export default function TruncatedText({
     className = '',
     text = '',
-    length = DEFAULT_MAX_TEXT_LENGTH
+    length = DEFAULT_MAX_TEXT_LENGTH_RATIO
 }) {
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+    // Обновляем ширину окна при изменении размера окна
+    useEffect(() => {
+        function handleResize() {
+            setWindowWidth(window.innerWidth);
+        }
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
 
     const truncate = () => {
-        if (text.length > length) {
-            return text.substring(0, length) + "...";
+        if (text.length > windowWidth / length) {
+            return text.substring(0, windowWidth / length) + "...";
         }
         return text;
     }
--- resources/js/constants.js
+++ resources/js/constants.js
@@ -1,6 +1,7 @@
 export const DELAY_AFTER_SUCCESSFULLY_ACTION = 1500;
-export const DEFAULT_MAX_TEXT_LENGTH = 22;
-export const MAX_BOOK_TITLE_LENGTH = 15;
+export const DEFAULT_MAX_TEXT_LENGTH_RATIO = 50;
+export const MAX_BOOK_TITLE_LENGTH_RATIO = 50;
+export const MAX_BOOK_AUTHOR_LENGTH_RATIO = 48;
 
 export const GRAPHIC_BOOK_TYPE = 0;
 export const DIGITAL_BOOK_TYPE = 1;
