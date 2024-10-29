f22242678bb7f4a91908f04b7c4d0f9ff65237df
Marator2K07 <dmitriyshaaa@gmail.com>
2024-10-29 16:06:26 +0300

    Создан отдельный класс для обработки запросов обновления авторов.

---
+++ app/Http/Requests/Author/AuthorUpdateRequest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Requests\Author;
+
+use Illuminate\Validation\Rules;
+use Illuminate\Foundation\Http\FormRequest;
+
+class AuthorUpdateRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'string|max:255|min:5|required',
+            'email' => 'required|string|email|max:255',
+            'current_password' => ['required', 'current_password'],
+            'password' => ['required', Rules\Password::defaults()],
+            'day_of_birth' => 'before:now|after:1970-01-01|date_format:Y-m-d|required'
+        ];
+    }
+}
