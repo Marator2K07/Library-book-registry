884c9ef2134ed57f4bfa19410562cba68973663d
Marator2K07 <dmitriyshaaa@gmail.com>
2024-10-30 17:48:15 +0300

    Создан компонент выбора единственной сущности из списка.
    тестовая версия.

---
+++ resources/js/Components/EntitySelector.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+
+export default function EntitySelector({
+    className = '',
+    entities = null,
+    onSelect = null,
+    emptyLabel = '...'
+}) {
+    const [selectedEntity, setSelectedEntity] = useState(null);
+
+    const handleSelect = (entity) => {
+        setSelectedEntity(entity);
+        onSelect(entity);
+    };
+
+    if (entities && onSelect && entities.length > 0) {
+        return (
+            <div style={{
+                height: '95px',
+                overflowY: 'auto'
+            }}>
+                <ul>
+                    {entities.map((entity) => (
+                        <li
+                            key={entity.id}
+                            onClick={() => handleSelect(entity)}
+                            style={{ backgroundColor: selectedEntity === entity ? 'red' : 'rgb(31,41,55)' }}
+                            className={selectedEntity === entity ? 'selected' : ''}
+                        >
+                            {entity.name}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        )
+    } else {
+        return (
+            <div className={className}>
+                <p>{emptyLabel}</p>
+            </div>
+        )
+    }
+}
